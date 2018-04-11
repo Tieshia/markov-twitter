82b4bb50330ed881fdee43f6bdebaea5036f780e
Hackbright Student <no-reply@hackbrightacademy.com>
2018-04-11 12:51:58 -0700

    Edited tweet() to allow for interactive loop.

--- markov.py
+++ markov.py
@@ -72,6 +72,22 @@ def tweet(chains):
     # Note: you must run `source secrets.sh` before running this file
     # to make sure these environmental variables are set.
 
+    # api = twitter.Api(
+    #     consumer_key=os.environ['TWITTER_CONSUMER_KEY'],
+    #     consumer_secret=os.environ['TWITTER_CONSUMER_SECRET'],
+    #     access_token_key=os.environ['TWITTER_ACCESS_TOKEN_KEY'],
+    #     access_token_secret=os.environ['TWITTER_ACCESS_TOKEN_SECRET'])
+
+    # api.VerifyCredentials()
+
+    continue_tweeting = True
+
+    while continue_tweeting != "q":
+        status = make_text(chains)
+        print status
+        continue_tweeting = raw_input("\nEnter to tweet again [q to quit] > ")
+
+
 # Get the filenames from the user through a command line prompt, ex:
 # python markov.py green-eggs.txt shakespeare.txt
 filenames = sys.argv[1:]
@@ -82,7 +98,5 @@ text = open_and_read_file(filenames)
 # Get a Markov chain
 chains = make_chains(text)
 # Your task is to write a new function tweet, that will take chains as input
-# tweet(chains)
-markov_tweet = make_text(chains)
-
-send_tweet = tweet(markov_tweet)
+# tweet(markov_tweet)
+send_tweet = tweet(chains)
